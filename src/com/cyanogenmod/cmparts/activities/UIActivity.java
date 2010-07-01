2bcfd630cad0f7d06880666c911d1732e5fb26b8
Jonas Larsson <jonas@hallerud.se>
2010-07-02 00:47:09 +0200

    Invidually configurable accelerometer rotation for 90, 180 and 270 degrees

--- src/com/cyanogenmod/cmparts/activities/UIActivity.java
+++ src/com/cyanogenmod/cmparts/activities/UIActivity.java
@@ -12,8 +12,14 @@ import com.cyanogenmod.cmparts.R;
 public class UIActivity extends PreferenceActivity {
 
     private static final String PINCH_REFLOW_PREF = "pref_pinch_reflow";    
+    private static final String ROTATION_90_PREF = "pref_rotation_90";
+    private static final String ROTATION_180_PREF = "pref_rotation_180";
+    private static final String ROTATION_270_PREF = "pref_rotation_270";
 
     private CheckBoxPreference mPinchReflowPref;
+    private CheckBoxPreference mRotation90Pref;
+    private CheckBoxPreference mRotation180Pref;
+    private CheckBoxPreference mRotation270Pref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,16 @@ public class UIActivity extends PreferenceActivity {
         addPreferencesFromResource(R.xml.ui_settings);
 
         PreferenceScreen prefSet = getPreferenceScreen();
+
+        /* Rotation */
+        mRotation90Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_90_PREF);
+        mRotation180Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_180_PREF);
+        mRotation270Pref = (CheckBoxPreference) prefSet.findPreference(ROTATION_270_PREF);
+        int mode = Settings.System.getInt(getContentResolver(),
+                        Settings.System.ACCELEROMETER_ROTATION_MODE, 5);
+        mRotation90Pref.setChecked((mode & 1) != 0);
+        mRotation180Pref.setChecked((mode & 2) != 0);
+        mRotation270Pref.setChecked((mode & 4) != 0);
 
         /* Pinch reflow */
         mPinchReflowPref = (CheckBoxPreference) prefSet.findPreference(PINCH_REFLOW_PREF);
@@ -36,6 +52,16 @@ public class UIActivity extends PreferenceActivity {
             value = mPinchReflowPref.isChecked();
             Settings.System.putInt(getContentResolver(),
                     Settings.System.WEB_VIEW_PINCH_REFLOW, value ? 1 : 0);
+        }
+        if (preference == mRotation90Pref ||
+            preference == mRotation180Pref ||
+            preference == mRotation270Pref) {
+            int mode = 0;
+            if (mRotation90Pref.isChecked()) mode |= 1;
+            if (mRotation180Pref.isChecked()) mode |= 2;
+            if (mRotation270Pref.isChecked()) mode |= 4;
+            Settings.System.putInt(getContentResolver(),
+                     Settings.System.ACCELEROMETER_ROTATION_MODE, mode);
         }
         return true;
     }
