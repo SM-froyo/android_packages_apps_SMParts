0a6634952c0e06e69f0ee9d0a2cfa2d97346c12b
Steve Kondik <shade@chemlab.org>
2010-07-07 03:28:53 -0400

    Cleanup menus and strings

--- src/com/cyanogenmod/cmparts/activities/InputActivity.java
+++ src/com/cyanogenmod/cmparts/activities/InputActivity.java
@@ -1,19 +1,15 @@
 package com.cyanogenmod.cmparts.activities;
 
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import com.cyanogenmod.cmparts.R;
+
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
 
-import com.cyanogenmod.cmparts.R;
-import com.cyanogenmod.cmparts.provider.SettingsProvider;
-
-public class InputActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
+public class InputActivity extends PreferenceActivity {
 
     private static final String TRACKBALL_WAKE_PREF = "pref_trackball_wake";
     private static final String TRACKBALL_UNLOCK_PREF = "pref_trackball_unlock";
@@ -22,12 +18,6 @@ public class InputActivity extends PreferenceActivity implements OnSharedPrefere
     private CheckBoxPreference mTrackballWakePref;
     private CheckBoxPreference mTrackballUnlockPref;
     private CheckBoxPreference mMenuUnlockPref;
-
-    private String[] DEFAULT_PROJECTION = new String[] {
-            SettingsProvider.Constants._ID,
-            SettingsProvider.Constants.KEY,
-            SettingsProvider.Constants.VALUE
-    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,69 +47,6 @@ public class InputActivity extends PreferenceActivity implements OnSharedPrefere
         mMenuUnlockPref.setChecked(Settings.System.getInt(getContentResolver(),
                 Settings.System.MENU_UNLOCK_SCREEN, 0) == 1);
 
-        final PreferenceGroup parentPreference = getPreferenceScreen();
-        parentPreference.getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
-    }
-
-    /*
-	private void writeToProvider(String key, String value) {
-		ContentValues values = new ContentValues();
-
-		values.put(Constants.KEY, key);
-		values.put(Constants.VALUE, value);
-
-		int id = getRowId(key);
-		if (id == -1) {
-			getContentResolver().insert(SettingsProvider.CONTENT_URI, values);
-		} else {
-			final String selection = Constants.KEY + "='" + key + "'";
-			Uri rowuri = Uri.parse("content://"+SettingsProvider.AUTHORITY+"/"+SettingsProvider.TABLE_NAME+"/"+id);
-			getContentResolver().update(rowuri, values, selection, null);
-		}
-	}
-
-	private int getInt(String key, int def) {
-		String value = getString(key);
-		try {
-			return value != null ? Integer.parseInt(value) : def;
-		} catch (NumberFormatException e) {
-			return def;
-		}
-	}
-
-	private String getString(String key) {
-		String value = null;
-		Cursor c = null;
-		String selection = "key='"+key+"'";
-		try {
-			c = managedQuery(SettingsProvider.CONTENT_URI, DEFAULT_PROJECTION, selection, null, null);
-			if (c != null && c.moveToNext()) {
-				value = c.getString(2);
-			}
-		} catch (SQLException e) {
-			// return null
-		} finally {
-			if (c != null) {
-				c.close();
-			}
-		}
-		return value;
-	}
-
-	private int getRowId(String key) {
-		String selection = "key='"+key+"'";
-		Cursor cur = managedQuery(SettingsProvider.CONTENT_URI, DEFAULT_PROJECTION, selection, null, null);
-		cur.moveToFirst();
-		if (cur.getCount() == 0) {
-			return -1;
-		} else {
-			return cur.getInt(0);
-		}
-	}
-     */
-
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-            String key) {
     }
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
@@ -128,15 +55,18 @@ public class InputActivity extends PreferenceActivity implements OnSharedPrefere
             value = mTrackballWakePref.isChecked();
             Settings.System.putInt(getContentResolver(),
                     Settings.System.TRACKBALL_WAKE_SCREEN, value ? 1 : 0);
+            return true;
         } else if (preference == mTrackballUnlockPref) {
             value = mTrackballUnlockPref.isChecked();
             Settings.System.putInt(getContentResolver(),
                     Settings.System.TRACKBALL_UNLOCK_SCREEN, value ? 1 : 0);
+            return true;
         } else if (preference == mMenuUnlockPref) {
             value = mMenuUnlockPref.isChecked();
             Settings.System.putInt(getContentResolver(),
                     Settings.System.MENU_UNLOCK_SCREEN, value ? 1 : 0);
+            return true;
         }
-        return true;
+        return false;
     }
 }
