0b8694aea2752fe56ff0b34be4f0bc137fb32563
Pedlar <pedlar88@gmail.com>
2010-07-23 21:38:39 -0400

    Added Trackball LED and Sucession

--- src/com/cyanogenmod/cmparts/activities/TrackballNotificationActivity.java
+++ src/com/cyanogenmod/cmparts/activities/TrackballNotificationActivity.java
@@ -4,7 +4,6 @@ import com.cyanogenmod.cmparts.R;
 
 import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -14,79 +13,23 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
-import java.io.Serializable;
+import java.io.File;
 import android.util.Log;
 import android.widget.Toast;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageInfo;
+import java.util.List;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 
 public class TrackballNotificationActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
 
 	private static final int NOTIFICATION_ID = 400;
-	
-	/* Color */
-	private static final String PREF_GOOGLE_VOICE_COLOR = "pref_google_voice_color";
-	private static final String PREF_MMS_COLOR = "pref_mms_color";
-	private static final String PREF_GMAIL_COLOR = "pref_gmail_color";
-	private static final String PREF_EMAIL_COLOR = "pref_email_color";
-	private static final String PREF_TWITTER_COLOR = "pref_twitter_color";
-	private static final String PREF_TWICCA_COLOR = "pref_twicca_color";
-	
-	/* Blink */
-	private static final String PREF_GOOGLE_VOICE_BLINK = "pref_google_voice_blink";
-	private static final String PREF_MMS_BLINK = "pref_mms_blink";
-	private static final String PREF_GMAIL_BLINK = "pref_gmail_blink";
-	private static final String PREF_EMAIL_BLINK = "pref_email_blink";
-	private static final String PREF_TWITTER_BLINK = "pref_twitter_blink";
-	private static final String PREF_TWICCA_BLINK = "pref_twicca_blink";
-	
-	/* Test */
-	private static final String PREF_GOOGLE_VOICE_TEST = "pref_google_voice_test";
-	private static final String PREF_MMS_TEST = "pref_mms_test";
-	private static final String PREF_GMAIL_TEST = "pref_gmail_test";
-	private static final String PREF_EMAIL_TEST = "pref_email_test";
-	private static final String PREF_TWITTER_TEST = "pref_twitter_test";
-	private static final String PREF_TWICCA_TEST = "pref_twicca_test";
-	
-	private static final String TRACKBALL_SCREEN_PREF = "pref_trackball_screen";
-	private static final String PREF_RESET = "pref_reset_notifcation";
-
-	/* Color */
-	private ListPreference mGoogleVoiceColor;
-	private ListPreference mMmsColor;
-	private ListPreference mGmailColor;
-	private ListPreference mEmailColor;
-	private ListPreference mTwitterColor;
-	private ListPreference mTwiccaColor;
-	
-	/* Blink Rate */
-	private ListPreference mGoogleVoiceBlink;
-	private ListPreference mMmsBlink;
-	private ListPreference mGmailBlink;
-	private ListPreference mEmailBlink;
-	private ListPreference mTwitterBlink;
-	private ListPreference mTwiccaBlink;
-	
-	/* Test */
-	private Preference mGoogleVoiceTest;
-	private Preference mMmsTest;
-	private Preference mGmailTest;
-	private Preference mEmailTest;
-	private Preference mTwitterTest;
-	private Preference mTwiccaTest;
-	
-    private CheckBoxPreference mTrackballScreenPref;
-    private Preference mReset;
 
 	public static String[] mPackage;
 	public String mPackageSource;
 	
-	
-
 	public boolean isNull(String mString) {
 		if(mString == null || mString.matches("null") 
 		|| mString.length() == 0
@@ -98,15 +41,13 @@ public class TrackballNotificationActivity extends PreferenceActivity implements
 		}
 	}
 
-	public static String[] getArray(String mGetFrom) {
-		if(mGetFrom == null || mGetFrom.matches("|") || mGetFrom.length() == 0) {
+	public String[] getArray(String mGetFrom) {
+		if(isNull(mGetFrom)) {
 			String[] tempfalse = new String[20];
 			return tempfalse;
 		}
 		String[] temp;
 		temp = mGetFrom.split("\\|");
-	//	for(int x = 0; x < temp.length; x++)
-			//Log.i("GetArray", "X="+x+" temp="+temp[x]);
 		return temp;
 	}
 
@@ -114,22 +55,19 @@ public class TrackballNotificationActivity extends PreferenceActivity implements
 		int i;
 		String temp = "";
 		for(i = 0; i < mArray.length; i++) {
-			if(mArray[i] == "" || mArray[i] == null || mArray[i].matches("null") || mArray[i].length() == 0)
+			if(isNull(mArray[i]))
 				continue;
-
 			temp = temp + "|" + mArray[i];
-			//Log.i("createString", "String:" +temp+ " mArray: " +mArray[i]);
 		}
 		return temp;
 	}
 
-	public static String[] getPackageAndColorAndBlink(String mString) {
-		if(mString == null || mString.matches("=") || mString.length() == 0 || mString.matches("null")) {
+	public String[] getPackageAndColorAndBlink(String mString) {
+		if(isNull(mString)) {
 			return null;
 		}
 		String[] temp;
 		temp = mString.split("=");
-		//Log.i("getPackageAndColor", "String: "+mString);
 		return temp;
 	}
 	
@@ -177,11 +115,15 @@ public class TrackballNotificationActivity extends PreferenceActivity implements
 			int x = 0;
 			//Get the last one
 			for(x = 0; x < temp.length; x++) {
-				//Log.i("addPackNew", "X="+x+" temp="+temp[x]);
-				if(temp[x] == null || temp[x].length() == 0 || temp[x].matches("null"))
+				if(isNull(temp[x]))
 					break;
 			}
-			String tempcolor = pkg+"="+color+"=2";
+			String tempcolor;
+			if(blink.matches("0")) {
+				tempcolor = pkg+"="+color+"=2";
+			} else {
+				tempcolor = pkg+"=black="+blink;
+			}
 			temp[x] = tempcolor;
 		}
 		Settings.System.putString(getContentResolver(), Settings.System.NOTIFICATION_PACKAGE_COLORS, createString(temp));
@@ -192,14 +134,15 @@ public class TrackballNotificationActivity extends PreferenceActivity implements
 		if(mTestPackage == null) {
 			return;
 		}
-		
 		final Notification notification = new Notification();
 		
 		notification.flags |= Notification.FLAG_SHOW_LIGHTS;
 		
+		int mBlinkRate = Integer.parseInt(mTestPackage[2]);
+		
 		notification.ledARGB = Color.parseColor(mTestPackage[1]);
         notification.ledOnMS = 500;
-        notification.ledOffMS = 2 * 1000;
+        notification.ledOffMS = mBlinkRate * 1000;
         NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         
         nm.notify(NOTIFICATION_ID, notification);
@@ -214,136 +157,149 @@ public class TrackballNotificationActivity extends PreferenceActivity implements
         } });
         endFlash.show();
 	}
+	
+	public String getPackageName(String pkg) {
+        PackageManager packageManager = getPackageManager(); 
+        List<PackageInfo> packs = packageManager.getInstalledPackages(0);
+        int size = packs.size();
+        for (int i = 0; i < size; i++) {
+        	PackageInfo p = packs.get(i);
+        	if(p.packageName.equals(pkg))
+        		return p.applicationInfo.loadLabel(packageManager).toString();
+        }
+        return null;
+	}
+	
+	public String[] getPackageList() {
+		PackageManager packageManager = getPackageManager(); 
+        List<PackageInfo> packs = packageManager.getInstalledPackages(0);
+        int size = packs.size();
+        String[] list = new String[30];
+        int x = 0;
+        for (int i = 0; i < size; i++) {
+        	PackageInfo p = packs.get(i);
+        	try {
+        		Context appContext = createPackageContext(p.packageName, 0);
+        		boolean exists = (new File(appContext.getFilesDir(), "trackball_lights")).exists(); 
+			if(exists) {
+        			list[x] = p.packageName;
+        			x++;
+        		}
+        	} catch (Exception e) {
+        		Log.d("GetPackageList", e.toString());
+        	}
+        }
+        return list;
+	}
 
+	private PreferenceScreen createPreferenceScreen() {
+		//The root of our system
+		String[] packageList = getPackageList();
+		PreferenceScreen root = getPreferenceManager().createPreferenceScreen(this);
+		
+        for(int i = 0; i < packageList.length; i++) {
+        	if(isNull(packageList[i]))
+        		continue;
+        	
+        	String packageName = getPackageName(packageList[i]);
+        	PreferenceScreen appName = getPreferenceManager().createPreferenceScreen(this);
+        	appName.setKey(packageList[i] + "_screen");
+        	appName.setTitle(packageName);
+        	root.addPreference(appName);
+        	
+        	ListPreference colorList = new ListPreference(this);
+        	colorList.setKey(packageList[i]);
+        	colorList.setTitle(R.string.color_trackball_flash);
+        	colorList.setSummary("Color to flash");
+        	colorList.setDialogTitle(R.string.dialog_color_trackball);
+        	colorList.setEntries(R.array.entries_trackball_colors);
+        	colorList.setEntryValues(R.array.pref_trackball_colors_values);
+        	colorList.setOnPreferenceChangeListener(this);
+        	appName.addPreference(colorList);
+        	
+        	ListPreference blinkList = new ListPreference(this);
+        	blinkList.setKey(packageList[i]);
+        	blinkList.setTitle(R.string.color_trackball_blink);
+        	blinkList.setSummary("Blink Rate");
+        	blinkList.setDialogTitle(R.string.dialog_blink_trackball);
+        	blinkList.setEntries(R.array.pref_trackball_blink_rate_entries);
+        	blinkList.setEntryValues(R.array.pref_trackball_blink_rate_values);
+        	blinkList.setOnPreferenceChangeListener(this);
+        	appName.addPreference(blinkList);
+        	
+        	Preference testColor = new Preference(this);
+        	testColor.setKey(packageList[i]);
+        	testColor.setSummary("Test the flash");
+        	testColor.setTitle(R.string.color_trackball_test);
+        	appName.addPreference(testColor);
+        }
+        
+        /*Advanced*/
+        PreferenceScreen advancedScreen = getPreferenceManager().createPreferenceScreen(this);
+        advancedScreen.setKey("advanced_screen");
+        advancedScreen.setTitle("Advanced");
+    	root.addPreference(advancedScreen);
+    	
+    	CheckBoxPreference alwaysPulse = new CheckBoxPreference(this);
+    	alwaysPulse.setKey("always_pulse");
+    	alwaysPulse.setSummary(R.string.pref_trackball_screen_summary);
+    	alwaysPulse.setTitle(R.string.pref_trackball_screen_title);
+    	advancedScreen.addPreference(alwaysPulse);
+    	
+    	//TRACKBALL_NOTIFICATION_SUCESSION
+    	CheckBoxPreference sucessionPulse = new CheckBoxPreference(this);
+    	sucessionPulse.setKey("pulse_sucession");
+    	sucessionPulse.setSummary(R.string.pref_trackball_sucess_summary);
+    	sucessionPulse.setTitle(R.string.pref_trackball_sucess_title);
+    	advancedScreen.addPreference(sucessionPulse);
+    	
+    	Preference resetColors = new Preference(this);
+    	resetColors.setKey("reset_notifications");
+    	resetColors.setSummary("Reset all colors and apps.");
+    	resetColors.setTitle("Reset");
+    	advancedScreen.addPreference(resetColors);
+    	
+        return root;
+	}
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+        super.onCreate(savedInstanceState);  
         setTitle(R.string.trackball_notifications_title);
-        addPreferencesFromResource(R.xml.trackball_notifications);
-
-        PreferenceScreen prefSet = getPreferenceScreen();
-        Boolean mCanEnableTrackball = (getResources().getBoolean(R.bool.has_trackball) == true);
-        
-        /* Colors */
-        mGoogleVoiceColor = (ListPreference) prefSet.findPreference(PREF_GOOGLE_VOICE_COLOR);
-        mGoogleVoiceColor.setOnPreferenceChangeListener(this);
-        mMmsColor = (ListPreference) prefSet.findPreference(PREF_MMS_COLOR);
-        mMmsColor.setOnPreferenceChangeListener(this);
-        mGmailColor = (ListPreference) prefSet.findPreference(PREF_GMAIL_COLOR);
-        mGmailColor.setOnPreferenceChangeListener(this);
-        mEmailColor = (ListPreference) prefSet.findPreference(PREF_EMAIL_COLOR);
-        mEmailColor.setOnPreferenceChangeListener(this);
-        mTwitterColor = (ListPreference) prefSet.findPreference(PREF_TWITTER_COLOR);
-        mTwitterColor.setOnPreferenceChangeListener(this);
-        mTwiccaColor = (ListPreference) prefSet.findPreference(PREF_TWICCA_COLOR);
-        mTwiccaColor.setOnPreferenceChangeListener(this);
-        
-        /* Blink */
-        mGoogleVoiceBlink = (ListPreference) prefSet.findPreference(PREF_GOOGLE_VOICE_BLINK);
-        mGoogleVoiceBlink.setOnPreferenceChangeListener(this);
-        mMmsBlink = (ListPreference) prefSet.findPreference(PREF_MMS_BLINK);
-        mMmsBlink.setOnPreferenceChangeListener(this);
-        mGmailBlink = (ListPreference) prefSet.findPreference(PREF_GMAIL_BLINK);
-        mGmailBlink.setOnPreferenceChangeListener(this);
-        mEmailBlink = (ListPreference) prefSet.findPreference(PREF_EMAIL_BLINK);
-        mEmailBlink.setOnPreferenceChangeListener(this);
-        mTwitterBlink = (ListPreference) prefSet.findPreference(PREF_TWITTER_BLINK);
-        mTwitterBlink.setOnPreferenceChangeListener(this);
-        mTwiccaBlink = (ListPreference) prefSet.findPreference(PREF_TWICCA_BLINK);
-        mTwiccaBlink.setOnPreferenceChangeListener(this);
-        
-        /* Test Color */
-        mGoogleVoiceTest = (Preference) prefSet.findPreference(PREF_GOOGLE_VOICE_TEST);
-        mMmsTest = (Preference) prefSet.findPreference(PREF_MMS_TEST);
-        mGmailTest = (Preference) prefSet.findPreference(PREF_GMAIL_TEST);
-        mEmailTest = (Preference) prefSet.findPreference(PREF_EMAIL_TEST);
-        mTwitterTest = (Preference) prefSet.findPreference(PREF_TWITTER_TEST);
-        mTwiccaTest = (Preference) prefSet.findPreference(PREF_TWICCA_TEST);
-        
-        mReset = (Preference) prefSet.findPreference(PREF_RESET);
-        
-        /* Trackball While Screen On */
-        mTrackballScreenPref = (CheckBoxPreference) prefSet.findPreference(TRACKBALL_SCREEN_PREF);
-        mTrackballScreenPref.setChecked(Settings.System.getInt(getContentResolver(),
-                Settings.System.TRACKBALL_SCREEN_ON, 0) == 1);
+        setPreferenceScreen(createPreferenceScreen());
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mGoogleVoiceColor) {
-            updatePackage("com.google.android.apps.googlevoice", objValue.toString(), "0");
+        String value = objValue.toString();
+        if(preference.getSummary() != null) {
+        	if(preference.getSummary().toString().contains("Blink")) {
+        		updatePackage(preference.getKey().toString(), "", value);
+        	} else {
+        		updatePackage(preference.getKey().toString(), value, "0");
+        	}
         }
-        if (preference == mGoogleVoiceBlink) {
-        	updatePackage("com.google.android.apps.googlevoice", "", objValue.toString());
-        }
-        
-        if (preference == mMmsColor) {
-        	updatePackage("com.android.mms", objValue.toString(), "0");
-        }
-        if (preference == mMmsBlink) {
-        	updatePackage("com.android.mms", "", objValue.toString());
-        }
-        
-        if (preference == mGmailColor) {
-        	updatePackage("com.google.android.gm", objValue.toString(), "0");
-        }
-        if (preference == mGmailBlink) {
-        	updatePackage("com.google.android.gm", "", objValue.toString());
-        }
-        
-        if (preference == mEmailColor) {
-        	updatePackage("com.android.email", objValue.toString(), "0");
-        }
-        if (preference == mEmailBlink) {
-        	updatePackage("com.android.email", "", objValue.toString());
-        }
-        
-        if (preference == mTwitterColor) {
-        	updatePackage("com.twitter.android", objValue.toString(), "0");
-        }
-        if (preference == mTwitterBlink) {
-        	updatePackage("com.twitter.android", "", objValue.toString());
-        }
-        
-        if(preference == mTwiccaColor) {
-        	updatePackage("jp.r246.twicca", objValue.toString(), "0");
-        }
-        if(preference == mTwiccaBlink) {
-        	updatePackage("jp.r246.twicca", "", objValue.toString());
-        }
-
-        // always let the preference setting proceed.
         return true;
     }
-    
+
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         boolean value;
-        if (preference == mTrackballScreenPref) {
-            value = mTrackballScreenPref.isChecked();
+        if (preference.getKey().toString().equals("reset_notifications")) {
+        	Settings.System.putString(getContentResolver(), Settings.System.NOTIFICATION_PACKAGE_COLORS, "");
+        	Toast.makeText(this, "Reset all colors", Toast.LENGTH_LONG).show();
+        } else if (preference.getKey().toString().equals("always_pulse")) {
+        	CheckBoxPreference keyPref = (CheckBoxPreference) preference;
+        	value = keyPref.isChecked();
             Settings.System.putInt(getContentResolver(),
                     Settings.System.TRACKBALL_SCREEN_ON, value ? 1 : 0);
-        }
-        else if (preference == mReset) {
-        	Settings.System.putString(getContentResolver(), Settings.System.NOTIFICATION_PACKAGE_COLORS, "");
-        	Toast.makeText(this, "Reset all colors", Toast.LENGTH_LONG);
-        } 
-        else if (preference == mGoogleVoiceTest) {
-        	testPackage("com.google.android.apps.googlevoice");
-        }
-        else if (preference == mMmsTest) {
-        	testPackage("com.android.mms");
-        }
-        else if (preference == mGmailTest) {
-        	testPackage("com.google.android.gm");
-        }
-        else if (preference == mEmailTest) {
-        	testPackage("com.android.email");
-        }
-        else if (preference == mTwitterTest) {
-        	testPackage("com.twitter.android");
-        }
-        else if(preference == mTwiccaTest) {
-        	testPackage("jp.r246.twicca");
+        } else if (preference.getKey().toString().equals("pulse_sucession")) {
+        		CheckBoxPreference keyPref = (CheckBoxPreference) preference;
+            	value = keyPref.isChecked();
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.TRACKBALL_NOTIFICATION_SUCESSION, value ? 1 : 0);
+        } else if(preference.getSummary() != null) {
+        	if(preference.getSummary().toString().equals("Test the flash")) {
+        		testPackage(preference.getKey().toString());
+        	}
         }
         return false;
     }
