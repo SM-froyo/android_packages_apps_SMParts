81ba5e111bdc53fd1c2694f1533cba47e3d31142
Steve Kondik <shade@chemlab.org>
2010-07-31 12:39:00 -0400

    perf: Add the "lock home in memory" option.

--- src/com/cyanogenmod/cmparts/activities/PerformanceSettingsActivity.java
+++ src/com/cyanogenmod/cmparts/activities/PerformanceSettingsActivity.java
@@ -11,6 +11,7 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
+import android.provider.Settings;
 
 import java.io.File;
 
@@ -46,17 +47,23 @@ public class PerformanceSettingsActivity extends PreferenceActivity implements P
     private static final String USE_DITHERING_PERSIST_PROP = "persist.sys.use_dithering";
     
     private static final String USE_DITHERING_DEFAULT = "1";
-    
+
+    private static final String LOCK_HOME_PREF = "pref_lock_home";
+
+    private static final int LOCK_HOME_DEFAULT = 0;
+
     private CheckBoxPreference mCompcachePref;
 
     private CheckBoxPreference mJitPref;
-    
+
     private CheckBoxPreference mUseDitheringPref;
-    
+
+    private CheckBoxPreference mLockHomePref;
+
     private ListPreference mHeapsizePref;
-    
+
     private AlertDialog alertDialog;
-    
+
     private int swapAvailable = -1;
     
     @Override
@@ -88,7 +95,11 @@ public class PerformanceSettingsActivity extends PreferenceActivity implements P
         mHeapsizePref.setValue(SystemProperties.get(HEAPSIZE_PERSIST_PROP, 
                 SystemProperties.get(HEAPSIZE_PROP, HEAPSIZE_DEFAULT)));
         mHeapsizePref.setOnPreferenceChangeListener(this);
-    
+
+        mLockHomePref = (CheckBoxPreference) prefSet.findPreference(LOCK_HOME_PREF);
+        mLockHomePref.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCK_HOME_IN_MEMORY, LOCK_HOME_DEFAULT) == 1);
+
         // Set up the warning
         alertDialog = new AlertDialog.Builder(this).create();
         alertDialog.setTitle(R.string.performance_settings_warning_title);
@@ -120,10 +131,16 @@ public class PerformanceSettingsActivity extends PreferenceActivity implements P
                     mUseDitheringPref.isChecked() ? "1" : "0");
             return true;
         }
-        
+
+        if (preference == mLockHomePref) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCK_HOME_IN_MEMORY, mLockHomePref.isChecked() ? 1 : 0);
+            return true;
+        }
+
         return false;
     }
-    
+
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mHeapsizePref) {
             if (newValue != null) {
