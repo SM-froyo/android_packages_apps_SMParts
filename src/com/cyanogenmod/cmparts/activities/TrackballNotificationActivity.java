6e96c350ef1c6b5cad4daeed688c41a4f7767b98
kmobs <persiansown@gmail.com>
2010-08-03 22:05:48 -0500

    Lots of new trackball notification options from Pedlar
    commit 271c4dfdf91b2816000eb0b745df4dd646f62785
    Author: Pedlar <pedlar88@gmail.com>
    Date:   Tue Aug 3 22:42:45 2010 -0400
    
        Added Warning Dialog
    
    commit 8c5e03b563eb6b429d2f34316732358373b2328b
    Merge: 0a9ca6e dc33596
    Author: Pedlar <pedlar88@gmail.com>
    Date:   Tue Aug 3 22:06:59 2010 -0400
    
        Merge branch 'master' of github.com:Pedlar/android_packages_apps_CMParts
    
        Conflicts:
        	res/values/strings.xml
    
    commit 0a9ca6ee7708315b40354ae2aa27cfc3fe5c95ab
    Merge: 5041f7c 555cd21
    Author: Pedlar <pedlar88@gmail.com>
    Date:   Tue Aug 3 21:59:18 2010 -0400
    
     Merge branch 'froyo' of github.com:Pedlar/android_packages_apps_CMParts into froyo
    
        Conflicts:
        	src/com/cyanogenmod/cmparts/activities/TrackballNotificationActivity.java
    
    commit 5041f7cb3064cde21ab7b1b8378294b6f8e49e31
    Author: Pedlar <pedlar88@gmail.com>
    Date:   Tue Aug 3 21:53:32 2010 -0400
    
        git add Battery
    
    commit e71b0c7c1184de283772122ea8110fb1eb72a298
    Author: Pedlar <pedlar88@gmail.com>
    Date:   Fri Jul 30 00:57:26 2010 -0400
    
        Trackball Modifications and a Battery Drain Tester
    
    commit 0b8694aea2752fe56ff0b34be4f0bc137fb32563
    Author: Pedlar <pedlar88@gmail.com>
    Date:   Fri Jul 23 21:38:39 2010 -0400
    
        Added Trackball LED and Sucession
    
    commit 22e0b3d515ea15bc671b2dd781fc123bf99f361a
    Author: Pedlar <pedlar88@gmail.com>
    Date:   Sat Jul 17 06:00:39 2010 -0400
    
        Trackball Support
    
    commit de24dd4c3d869bd87da3060f35f7c32985992108
    Author: Pedlar <pedlar88@gmail.com>
    Date:   Thu Jul 15 23:10:22 2010 -0400
    
        Added Trackball Notification Colors
    
    commit 8630b77ad05d70060e38ea09f12c0d62c2582525
    Author: Pedlar <pedlar88@gmail.com>
    Date:   Thu Jul 15 00:05:32 2010 -0400
    
        Trackball Notification Colors
    
    commit 326d38293a4ca6c37d7027531e09e039c341f8ce
    Author: Pedlar <pedlar88@gmail.com>
    Date:   Sat Jul 10 22:55:24 2010 -0400
    
        Added TrackBall Notification Activity file.
    
    commit 555cd21ba9a499d76345ae776c86dd7952dded20
    Author: Pedlar <pedlar88@gmail.com>
    Date:   Sat Jul 10 22:55:24 2010 -0400
    
        Added TrackBall Notification Activity file.
    
    Missing slash
    
    Fixed building issue.

---
+++ src/com/cyanogenmod/cmparts/activities/TrackballNotificationActivity.java
@@ -0,0 +1,430 @@
+package com.cyanogenmod.cmparts.activities;
+
+import com.cyanogenmod.cmparts.R;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import java.io.File;
+import android.util.Log;
+import android.widget.Toast;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageInfo;
+import java.util.List;
+import java.util.Random;
+
+public class TrackballNotificationActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+
+	private static final int NOTIFICATION_ID = 400;
+
+	public static String[] mPackage;
+	public String mPackageSource;
+
+	public boolean isNull(String mString) {
+		if(mString == null || mString.matches("null")
+		|| mString.length() == 0
+		|| mString.matches("|")
+		|| mString.matches("")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public String[] getArray(String mGetFrom) {
+		if(isNull(mGetFrom)) {
+			String[] tempfalse = new String[20];
+			return tempfalse;
+		}
+		String[] temp;
+		temp = mGetFrom.split("\\|");
+		return temp;
+	}
+
+	public String createString(String[] mArray) {
+		int i;
+		String temp = "";
+		for(i = 0; i < mArray.length; i++) {
+			if(isNull(mArray[i]))
+				continue;
+			temp = temp + "|" + mArray[i];
+		}
+		return temp;
+	}
+
+	public String[] getPackageAndColorAndBlink(String mString) {
+		if(isNull(mString)) {
+			return null;
+		}
+		String[] temp;
+		temp = mString.split("=");
+		return temp;
+	}
+
+	public String[] findPackage(String pkg) {
+		String mBaseString = Settings.System.getString(getContentResolver(), Settings.System.NOTIFICATION_PACKAGE_COLORS);
+		String[] mBaseArray = getArray(mBaseString);
+		for(int i = 0; i < mBaseArray.length; i++) {
+			if(isNull(mBaseArray[i])) {
+				continue;
+			}
+			if(mBaseArray[i].contains(pkg)) {
+				return getPackageAndColorAndBlink(mBaseArray[i]);
+			}
+		}
+		return null;
+	}
+
+	public void updatePackage(String pkg, String color, String blink) {
+		String stringtemp = Settings.System.getString(getContentResolver(), Settings.System.NOTIFICATION_PACKAGE_COLORS);
+		String[] temp = getArray(stringtemp);
+		int i;
+		String[] temp2;
+		temp2 = new String[temp.length];
+		boolean found = false;
+		for(i = 0; i < temp.length; i++) {
+			temp2 = getPackageAndColorAndBlink(temp[i]);
+			if(temp2 == null) {
+				continue;
+			}
+			if(temp2[0].matches(pkg)) {
+				if(blink.matches("0")) {
+					temp2[1] = color;
+				} else {
+					temp2[2] = blink;
+				}
+				found = true;
+				break;
+			}
+		}
+		if(found) {
+			String tempcolor = temp2[0] +"="+temp2[1]+"="+temp2[2];
+			temp[i] = tempcolor;
+		} else {
+			int x = 0;
+			//Get the last one
+			for(x = 0; x < temp.length; x++) {
+				if(isNull(temp[x]))
+					break;
+			}
+			String tempcolor;
+			if(blink.matches("0")) {
+				tempcolor = pkg+"="+color+"=2";
+			} else {
+				tempcolor = pkg+"=black="+blink;
+			}
+			temp[x] = tempcolor;
+		}
+		Settings.System.putString(getContentResolver(), Settings.System.NOTIFICATION_PACKAGE_COLORS, createString(temp));
+	}
+
+	private String[] colorList = {"green", "white", "red", "blue", "yellow", "cyan", "#800080", "#ffc0cb", "#ffa500", "#add8e6"};
+
+	public void testPackage(String pkg) {
+		String[] mTestPackage = findPackage(pkg);
+		if(mTestPackage == null) {
+			return;
+		}
+		final Notification notification = new Notification();
+		notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+		int mBlinkRate = Integer.parseInt(mTestPackage[2]);
+
+		if(mTestPackage[1].equals("random")) {
+			Random generator = new Random();
+                        int x = generator.nextInt(colorList.length - 1);
+                        notification.ledARGB = Color.parseColor(colorList[x]);
+                } else {
+			notification.ledARGB = Color.parseColor(mTestPackage[1]);
+		}
+
+	        notification.ledOnMS = 500;
+		notification.ledOffMS = mBlinkRate * 1000;
+		NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+	        nm.notify(NOTIFICATION_ID, notification);
+
+        	AlertDialog.Builder endFlash = new AlertDialog.Builder(this);
+        	endFlash.setMessage("Clear Flash")
+        	.setCancelable(false)
+        	.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+          	public void onClick(DialogInterface dialog, int which) {
+        	  	NotificationManager dialogNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+	        	  dialogNM.cancel(NOTIFICATION_ID);
+       		 } });
+	        endFlash.show();
+	}
+
+
+        public String knownPackage(String pkg) {
+                if(pkg.equals("com.android.email"))
+                        return "Email";
+                else if(pkg.equals("com.android.mms"))
+                        return "Messaging";
+                else if(pkg.equals("com.google.android.apps.googlevoice"))
+                        return "Google Voice";
+                else if(pkg.equals("com.google.android.gm"))
+                        return "Gmail";
+                else if(pkg.equals("com.google.android.gsf"))
+                        return "GTalk";
+                else if(pkg.equals("com.tiwtter.android"))
+                        return "Twitter";
+                else if(pkg.equals("jp.r246.twicca"))
+                        return "Twicca";
+
+                return null;
+        }
+
+	public String getPackageName(String pkg) {
+	        PackageManager packageManager = getPackageManager(); 
+		List<PackageInfo> packs = packageManager.getInstalledPackages(0);
+        	int size = packs.size();
+        	for (int i = 0; i < size; i++) {
+        		PackageInfo p = packs.get(i);
+        		if(p.packageName.equals(pkg)) {
+				if(knownPackage(pkg) != null) {
+					return knownPackage(pkg);
+				} else {
+        				return p.applicationInfo.loadLabel(packageManager).toString();
+				}
+			}
+        	}
+        	return null;
+	}
+
+	public String[] getPackageList() {
+		PackageManager packageManager = getPackageManager(); 
+	        List<PackageInfo> packs = packageManager.getInstalledPackages(0);
+        	int size = packs.size();
+        	String[] list = new String[50];
+        	int x = 0;
+        	for (int i = 0; i < size; i++) {
+        		PackageInfo p = packs.get(i);
+        		try {
+        			Context appContext = createPackageContext(p.packageName, 0);
+        			boolean exists = (new File(appContext.getFilesDir(), "trackball_lights")).exists(); 
+				if(exists || (knownPackage(p.packageName) != null)) {
+        				list[x] = p.packageName;
+        				x++;
+        			}
+        		} catch (Exception e) {
+        			Log.d("GetPackageList", e.toString());
+        		}
+        	}
+        	return list;
+	}
+
+	private PreferenceScreen createPreferenceScreen() {
+		//The root of our system
+		String[] packageList = getPackageList();
+		PreferenceScreen root = getPreferenceManager().createPreferenceScreen(this);
+
+		for(int i = 0; i < packageList.length; i++) {
+        		if(isNull(packageList[i]))
+        			continue;
+
+        	String packageName = getPackageName(packageList[i]);
+        	PreferenceScreen appName = getPreferenceManager().createPreferenceScreen(this);
+        	appName.setKey(packageList[i] + "_screen");
+        	appName.setTitle(packageName);
+        	root.addPreference(appName);
+
+        	ListPreference colorList = new ListPreference(this);
+        	colorList.setKey(packageList[i]);
+        	colorList.setTitle(R.string.color_trackball_flash);
+        	colorList.setSummary("Color to flash");
+        	colorList.setDialogTitle(R.string.dialog_color_trackball);
+        	colorList.setEntries(R.array.entries_trackball_colors);
+        	colorList.setEntryValues(R.array.pref_trackball_colors_values);
+        	colorList.setOnPreferenceChangeListener(this);
+        	appName.addPreference(colorList);
+
+        	ListPreference blinkList = new ListPreference(this);
+        	blinkList.setKey(packageList[i]);
+        	blinkList.setTitle(R.string.color_trackball_blink);
+        	blinkList.setSummary("Blink Rate");
+        	blinkList.setDialogTitle(R.string.dialog_blink_trackball);
+        	blinkList.setEntries(R.array.pref_trackball_blink_rate_entries);
+        	blinkList.setEntryValues(R.array.pref_trackball_blink_rate_values);
+        	blinkList.setOnPreferenceChangeListener(this);
+        	appName.addPreference(blinkList);
+
+        	Preference testColor = new Preference(this);
+        	testColor.setKey(packageList[i]);
+        	testColor.setSummary("Test the flash");
+        	testColor.setTitle(R.string.color_trackball_test);
+        	appName.addPreference(testColor);
+        }
+
+        /*Advanced*/
+        PreferenceScreen advancedScreen = getPreferenceManager().createPreferenceScreen(this);
+        advancedScreen.setKey("advanced_screen");
+        advancedScreen.setTitle("Advanced");
+    	root.addPreference(advancedScreen);
+
+    	CheckBoxPreference alwaysPulse = new CheckBoxPreference(this);
+    	alwaysPulse.setKey("always_pulse");
+    	alwaysPulse.setSummary(R.string.pref_trackball_screen_summary);
+    	alwaysPulse.setTitle(R.string.pref_trackball_screen_title);
+    	advancedScreen.addPreference(alwaysPulse);
+
+    	//TRACKBALL_NOTIFICATION_SUCESSION
+    	CheckBoxPreference sucessionPulse = new CheckBoxPreference(this);
+    	sucessionPulse.setKey("pulse_sucession");
+    	sucessionPulse.setSummary(R.string.pref_trackball_sucess_summary);
+    	sucessionPulse.setTitle(R.string.pref_trackball_sucess_title);
+    	advancedScreen.addPreference(sucessionPulse);
+
+        //TRACKBALL_NOTIFICATION_SUCESSION
+        CheckBoxPreference randomPulse = new CheckBoxPreference(this);
+        randomPulse.setKey("pulse_random_colors");
+        randomPulse.setSummary("Random colors");
+        randomPulse.setTitle("Pulses the LED in random colors for all notifications");
+        advancedScreen.addPreference(randomPulse);
+
+        //TRACKBALL_NOTIFICATION_SUCESSION
+        CheckBoxPreference orderPulse = new CheckBoxPreference(this);
+        orderPulse.setKey("pulse_colors_in_order");
+        orderPulse.setSummary("Pulse in order");
+        orderPulse.setTitle("Pulses the LED color in order.");
+        advancedScreen.addPreference(orderPulse);
+
+    	Preference resetColors = new Preference(this);
+    	resetColors.setKey("reset_notifications");
+    	resetColors.setSummary("Reset all colors and apps.");
+    	resetColors.setTitle("Reset");
+    	advancedScreen.addPreference(resetColors);
+
+        return root;
+	}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);  
+        setTitle(R.string.trackball_notifications_title);
+        setPreferenceScreen(createPreferenceScreen());
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        String value = objValue.toString();
+        if(preference.getSummary() != null) {
+        	if(preference.getSummary().toString().contains("Blink")) {
+        		updatePackage(preference.getKey().toString(), "", value);
+        	} else {
+        		updatePackage(preference.getKey().toString(), value, "0");
+        	}
+        }
+        return true;
+    }
+
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        final boolean value;
+	AlertDialog alertDialog;
+        if (preference.getKey().toString().equals("reset_notifications")) {
+        	Settings.System.putString(getContentResolver(), Settings.System.NOTIFICATION_PACKAGE_COLORS, "");
+        	Toast.makeText(this, "Reset all colors", Toast.LENGTH_LONG).show();
+        } else if (preference.getKey().toString().equals("always_pulse")) {
+        	CheckBoxPreference keyPref = (CheckBoxPreference) preference;
+        	value = keyPref.isChecked();
+                Settings.System.putInt(getContentResolver(),
+                Settings.System.TRACKBALL_SCREEN_ON, value ? 1 : 0);
+        } else if (preference.getKey().toString().equals("pulse_sucession")) {
+       		final CheckBoxPreference keyPref = (CheckBoxPreference) preference;
+            	value = keyPref.isChecked();
+                if(value == false) {
+                        Settings.System.putInt(getContentResolver(),
+                        Settings.System.TRACKBALL_NOTIFICATION_SUCESSION, 0);
+                        return true;
+                }
+
+		alertDialog = new AlertDialog.Builder(this).create();
+       		alertDialog.setTitle(R.string.notification_battery_warning_title);
+       		alertDialog.setMessage(getResources().getString(R.string.notification_battery_warning));
+       		alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Ok", new DialogInterface.OnClickListener() {
+     		public void onClick(DialogInterface dialog, int which) {
+               		Settings.System.putInt(getContentResolver(),
+                       		Settings.System.TRACKBALL_NOTIFICATION_SUCESSION, value ? 1 : 0);
+			return;
+      		} });
+		alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                        Settings.System.putInt(getContentResolver(),
+                           Settings.System.TRACKBALL_NOTIFICATION_SUCESSION, value ? 1 : 0);
+               		keyPref.setChecked(false);
+		         return;
+                }});
+	        alertDialog.show();
+	} else if (preference.getKey().toString().equals("pulse_random_colors")) {
+                final CheckBoxPreference keyPref = (CheckBoxPreference) preference;
+                value = keyPref.isChecked();
+                if(value == false) {
+                        Settings.System.putInt(getContentResolver(),
+                        Settings.System.TRACKBALL_NOTIFICATION_RANDOM, 0);
+                        return true;
+                }
+
+                alertDialog = new AlertDialog.Builder(this).create();
+                alertDialog.setTitle(R.string.notification_battery_warning_title);
+                alertDialog.setMessage(getResources().getString(R.string.notification_battery_warning));
+                alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+	                Settings.System.putInt(getContentResolver(),
+                        Settings.System.TRACKBALL_NOTIFICATION_RANDOM, value ? 1 : 0);
+                        return;
+
+                } });
+                alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+        	        Settings.System.putInt(getContentResolver(),
+                        Settings.System.TRACKBALL_NOTIFICATION_RANDOM, 0);
+			keyPref.setChecked(false);
+                        return;
+                }});
+                alertDialog.show();
+	} else if (preference.getKey().toString().equals("pulse_colors_in_order")) {
+                final CheckBoxPreference keyPref = (CheckBoxPreference) preference;
+                value = keyPref.isChecked();
+		if(value == false) {
+			Settings.System.putInt(getContentResolver(),
+                        Settings.System.TRACKBALL_NOTIFICATION_PULSE_ORDER, 0);
+			return true;
+		}
+
+		alertDialog = new AlertDialog.Builder(this).create();
+                alertDialog.setTitle(R.string.notification_battery_warning_title);
+                alertDialog.setMessage(getResources().getString(R.string.notification_battery_warning));
+                alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+        	        Settings.System.putInt(getContentResolver(),
+                        Settings.System.TRACKBALL_NOTIFICATION_PULSE_ORDER, value ? 1 : 0);
+                        return;
+                } });
+                alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+	                Settings.System.putInt(getContentResolver(),
+                        Settings.System.TRACKBALL_NOTIFICATION_PULSE_ORDER, 0);
+			keyPref.setChecked(false);
+                        return;
+                }});
+                alertDialog.show();
+
+
+        } else if(preference.getSummary() != null) {
+        	if(preference.getSummary().toString().equals("Test the flash")) {
+        		testPackage(preference.getKey().toString());
+        	}
+        }
+        return false;
+    }
+}
