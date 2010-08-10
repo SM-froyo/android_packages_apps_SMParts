7646203e9b250d0e3467df7288a9036db73fb9ce
Pedlar <pedlar88@gmail.com>
2010-08-10 00:55:42 -0400

    Custom Color Dialog Picker

--- src/com/cyanogenmod/cmparts/activities/TrackballNotificationActivity.java
+++ src/com/cyanogenmod/cmparts/activities/TrackballNotificationActivity.java
@@ -33,6 +33,7 @@ public class TrackballNotificationActivity extends PreferenceActivity implements
 	public String mPackageSource;
 	public Handler mHandler = new Handler();
 	public ProgressDialog pbarDialog;
+	public String mGlobalPackage;
 
 	public boolean isNull(String mString) {
 		if(mString == null || mString.matches("null")
@@ -276,6 +277,12 @@ public class TrackballNotificationActivity extends PreferenceActivity implements
         	blinkList.setOnPreferenceChangeListener(this);
         	appName.addPreference(blinkList);
 
+                Preference customColor = new Preference(this);
+                customColor.setKey(packageList[i]+"_custom");
+                customColor.setSummary(R.string.color_trackball_custom_summary);
+                customColor.setTitle(R.string.color_trackball_custom_title);
+                appName.addPreference(customColor);
+
         	Preference testColor = new Preference(this);
                 testColor.setKey(packageList[i]+"_test");
         	testColor.setSummary(R.string.color_trackball_test_summary);
@@ -354,9 +361,10 @@ public class TrackballNotificationActivity extends PreferenceActivity implements
         String pkg = key.substring(0, key.lastIndexOf("_"));
         if(key.endsWith("_blink")) {
             updatePackage(pkg, "", value);
-        } else {
+        } else if (key.endsWith("_color")) {
             updatePackage(pkg, value, "0");
-        }
+	}
+
         return true;
     }
 
@@ -454,10 +462,61 @@ public class TrackballNotificationActivity extends PreferenceActivity implements
                 final CheckBoxPreference keyPref = (CheckBoxPreference) preference;
                 value = keyPref.isChecked();
                 Settings.System.putInt(getContentResolver(), Settings.System.TRACKBALL_NOTIFICATION_BLEND_COLOR, value ? 1 : 0);
+	} else if(preference.getKey().toString().endsWith("_custom")) {
+            String pkg = preference.getKey().toString().substring(0, preference.getKey().toString().lastIndexOf("_"));
+	    mGlobalPackage = pkg;
+            ColorPickerDialog cp = new ColorPickerDialog(this,
+                   mPackageColorListener,
+                   readPackageColor());
+            cp.show();
         } else if(preference.getKey().toString().endsWith("_test")) {
             String pkg = preference.getKey().toString().substring(0, preference.getKey().toString().lastIndexOf("_"));
             testPackage(pkg);
         }
         return false;
     }
+
+    private int readPackageColor() {
+	     String[] mPackage = findPackage(mGlobalPackage);
+	     if(mPackage == null) {
+		return -16777216;
+	     }
+	     if(mPackage[1].equals("random")) {
+		return -16777216;
+	     } else {
+		return Color.parseColor(mPackage[1]);
+	     }
+    }
+    ColorPickerDialog.OnColorChangedListener mPackageColorListener =
+        new ColorPickerDialog.OnColorChangedListener() {
+            public void colorChanged(int color) {
+               updatePackage(mGlobalPackage, convertToARGB(color), "0");
+            }
+    };
+
+    private String convertToARGB(int color) {
+	String alpha = Integer.toHexString(Color.alpha(color));
+        String red = Integer.toHexString(Color.red(color));
+        String green = Integer.toHexString(Color.green(color));
+        String blue = Integer.toHexString(Color.blue(color));
+
+	if (alpha.length() == 1) {
+            alpha = "0" + alpha;
+        }
+
+	if (red.length() == 1) {
+            red = "0" + red;
+        }
+
+        if (green.length() == 1) {
+            green = "0" + green;
+        }
+
+        if (blue.length() == 1) {
+            blue = "0" + blue;
+        }
+
+        return "#" + alpha + red + green + blue;
+    }
+
 }
