3ae1c295bfa16090c0d71286728174a00e514ef4
Eran <eranmizra@gmail.com>
2010-09-29 04:23:52 +0200

    Disable surface dithering by default

--- src/com/cyanogenmod/cmparts/activities/PerformanceSettingsActivity.java
+++ src/com/cyanogenmod/cmparts/activities/PerformanceSettingsActivity.java
@@ -46,7 +46,7 @@ public class PerformanceSettingsActivity extends PreferenceActivity implements P
     
     private static final String USE_DITHERING_PERSIST_PROP = "persist.sys.use_dithering";
     
-    private static final String USE_DITHERING_DEFAULT = "1";
+    private static final String USE_DITHERING_DEFAULT = "0";
 
     private static final String LOCK_HOME_PREF = "pref_lock_home";
 
